1fd0b99d943ec7aff5c7877171be599e9876fd3f
Segrey Agapov <stigrov@ya.ru>
2024-02-22 18:56:00 +0300

    add vacancies.py

---
+++ src/vacancies.py
@@ -0,0 +1,17 @@
+class Vacancies:
+    def __init__(self, name, page):
+        self.__name = name
+        self.__page = page
+
+    @property
+    def name(self):
+        return self.__name
+
+    def page(self):
+        return self.__page
+
+    def __str__(self):
+        return f"{self.__name}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.__name, self.__page}"
